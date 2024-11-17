7fa92819fb5ff73dbff3602c7e51c7f42a866b08
Russ Green <1886088+russgreen@users.noreply.github.com>
2024-11-17 10:36:38 +0000

    Remove outdated configs and add error handling
    Removed configurations for release versions R22-R25 and related GUID from FileUpgrader.sln. Removed SolutionProperties and ExtensibilityGlobals sections. Added error handling in Build.Installer.cs to throw an exception if Advanced Installer CLI process fails.

--- build/Build.Installer.cs
+++ build/Build.Installer.cs
@@ -56,6 +56,11 @@ partial class Build
 
         var process = Process.Start(p);
         process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception("Advanced Installer CLI failed.");
+        }
     }
 
     static string GetAdvancedInstallerPath()
